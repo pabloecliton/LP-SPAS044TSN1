86e094603905b0b7d46f1741ee4f64eb94c23fe1
unknown <pabloecliton.facens@gmail.com>
2025-04-29 20:55:06 -0300

    LP

--- AC2/exc01-ac2.js
+++ AC2/exc01-ac2.js
@@ -2,14 +2,20 @@ function checarNumeros() {
 
 var x = document.getElementById("E1");
 var y = Math.random() * 10;
+var p = document.createElement('p');
+var resultado = document.getElementById('resultado');
 
   if (parseFloat(x.value) == y) {
-    var p = document.createElement('p');
     p.innerHTML = "Você acertou o número: " + y;
-    document.getElementById('resultado').appendChild(p);
+    resultado.innerHTML = "";
+    resultado.appendChild(p);
   } else if (x.value < y) {
-    alert("Muito baixo, tente novamente");
+    p.innerHTML = "Muito baixo, tente novamente";
+    resultado.innerHTML = "";
+    resultado.appendChild(p);
   } else {
-    alert("Muito alto, quem sabe na próxima");
+    p.innerHTML = "Muito alto, quem sabe na próxima";
+    resultado.innerHTML = "";
+    resultado.appendChild(p);
   }
 }
